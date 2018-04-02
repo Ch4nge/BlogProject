bfc80d8cde8d21090b58d404bacfd71109038d9f
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-04-02 17:58:01 +0300

    Added Akios styles

--- blog/src/comments/Comment.js
+++ blog/src/comments/Comment.js
@@ -4,16 +4,14 @@ export default class Comment extends Component {
 
     render(){
         return(
-            <div className="card">
-                <div className="commentHeader">
-                    <h4>{this.props.comment.userName}</h4>
-                    <h5><span className="articleDate">18.3.2018 15:00</span></h5>
-                </div>
+            <div className="commentCard">
+                <span className="userName">{this.props.comment.userName}</span>
                 <div className="commentContent">
                     <p>
                         {this.props.comment.content}
                     </p>
                 </div>
+                <span className="commentDate">18.3.2018 15:00</span>
             </div>
         );
         
