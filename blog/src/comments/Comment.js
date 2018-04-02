5ad80113d71659045666b0f0607ca92c374991ff
Akio Ide <akio.ide@cs.tamk.fi>
2018-04-02 17:38:57 +0300

    comment design improved.

--- blog/src/comments/Comment.js
+++ blog/src/comments/Comment.js
@@ -5,16 +5,15 @@ export default class Comment extends Component {
     render(){
         console.log(this.props.article);
         return(
-            <div className="card">
-                <div className="commentHeader">
-                    <h4>{this.props.comment.username}</h4>
-                    <h5><span className="articleDate">18.3.2018 15:00</span></h5>
-                </div>
+            <div className="commentCard">
+                <span className="userName">{this.props.comment.username}</span>
                 <div className="commentContent">
                     <p>
                         {this.props.comment.content}
                     </p>
+                    
                 </div>
+                <span className="commentDate">18.3.2018 15:00</span>
             </div>
         );
         
