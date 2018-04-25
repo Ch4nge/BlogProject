2e449b2791623277b6ca03707a323ca014402dd8
Akio Ide <akio.ide@cs.tamk.fi>
2018-04-25 22:44:14 +0300

    only admin can see deletea and add article

--- blog/src/articles/ArticleForm.js
+++ blog/src/articles/ArticleForm.js
@@ -62,19 +62,19 @@ export default class ArticleForm extends Component {
           description.length > 0 &&
           content.length > 0;
     return(
-      <div class="modal fade" id="postNotificationModal" style={{width:'100%', margin:'auto'}}>
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">New Article</h5>
+      <div className="modal fade" id="postNotificationModal" style={{width:'100%', margin:'auto'}}>
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">New Article</h5>
               
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
               <form>
-                <div class="form-group row">
-                  <label for="title" class="col-sm-2 col-form-label">Title:</label>
-                  <div class="col-sm-10">
-                    <input class="form-control" input type="text" 
+                <div className="form-group row">
+                  <label className="col-sm-2 col-form-label">Title:</label>
+                  <div className="col-sm-10">
+                    <input className="form-control" input type="text" 
                           name="title" 
                           ref="title" 
                           onChange={this.handleChange} 
@@ -82,10 +82,10 @@ export default class ArticleForm extends Component {
                           placeholder="Title of Article" required/>
                   </div>
                 </div>
-                <div class="form-group row">
-                  <label for="description" class="col-sm-2 col-form-label">Description:</label>
-                  <div class="col-sm-10">
-                    <input class="form-control"
+                <div className="form-group row">
+                  <label className="col-sm-2 col-form-label">Description:</label>
+                  <div className="col-sm-10">
+                    <input className="form-control"
                             input type="text" 
                             name="description" 
                             ref = "description"
@@ -94,10 +94,10 @@ export default class ArticleForm extends Component {
                             placeholder="Description"required/>
                   </div>
                 </div>
-                <div class="form-group row">
-                  <label for="content" class="col-sm-2 col-form-label">Content:</label>
-                  <div class="col-sm-10">
-                    <textarea class="form-control" 
+                <div className="form-group row">
+                  <label className="col-sm-2 col-form-label">Content:</label>
+                  <div className="col-sm-10">
+                    <textarea className="form-control" 
                       rows="20" cols="40" 
                       name="content"
                       ref="content" 
@@ -106,21 +106,21 @@ export default class ArticleForm extends Component {
                       placeholder="Your article..." required/>
                   </div>
                 </div>
-                <div class="form-group row">
-                  <label for="addTags" class="col-sm-2 col-form-label">Add Tags: </label>
-                  <div class="col-sm-5">
-                  <input class="form-control"
+                <div className="form-group row">
+                  <label className="col-sm-2 col-form-label">Add Tags: </label>
+                  <div className="col-sm-5">
+                  <input className="form-control"
                                     type="text"
                                     name="tagText"
                                     ref="tagText"
                                     onChange={this.handleChange}/>
                   </div>
-                  <button type="button"class="btn btn-default" onClick={(event) => this.addTag(event, this.state.tagText) }>Add</button>
+                  <button type="button"className="btn btn-default" onClick={(event) => this.addTag(event, this.state.tagText) }>Add</button>
 
                 </div>
-                <div class="form-group row">
-                    <label for="addedTags" class="col-sm-2 col-form-label"></label>
-                    <div class="col-sm-5">
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label"></label>
+                    <div className="col-sm-5">
                     <div className="formTags">
                         {this.state.tags.map((tag) =>{
                             return <span className="tag">{tag.title}</span>
@@ -130,12 +130,12 @@ export default class ArticleForm extends Component {
                   </div>
               </form>
             </div>
-            <div class="modal-footer">
-              <button class="btn btn-secondary" 
+            <div className="modal-footer">
+              <button className="btn btn-secondary" 
                       data-dismiss="modal">Cancel</button>
-              <button class="btn btn-primary" 
+              <button className="btn btn-primary" 
                       disabled={!enabled} 
-                      class="btn btn-primary" 
+                      className="btn btn-primary" 
                       onClick={this.postArticle} 
                       data-dismiss="modal">Post</button>
             </div>
